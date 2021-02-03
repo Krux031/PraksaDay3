ba39c3f7de368be676aeb49795e1133d49d3b8db
Krux031 <49514324+Krux031@users.noreply.github.com>
2021-02-03 21:56:35 +0100

    izmjena

--- PraksaDay2/Controllers/HelloController.cs
+++ PraksaDay2/Controllers/HelloController.cs
@@ -15,7 +15,7 @@ namespace PraksaDay2.Controllers
 
         SqlCommand dohvati = null;
         SqlTransaction transaction;
-        public static SqlConnection konekcija = new SqlConnection(@"Server=tcp:kruninserver.database.windows.net,1433;Initial Catalog=kruninabaza;Persist Security Info=False;User ID=krux031;Password=Desetisesti13;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        public static SqlConnection konekcija = new SqlConnection(@"Server=tcp:kruninserver.database.windows.net,1433;Initial Catalog=kruninabaza;Persist Security Info=False;User ID=krux031;Password=sifra;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         public HttpResponseMessage Get()
         {
